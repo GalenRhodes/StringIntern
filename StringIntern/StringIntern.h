fbd5f88fae55903288d37db25fcdc444264bc74b
Galen Rhodes <galenrhodes@me.com>
2021-12-15 08:32:52 -0500

    Initial Commit

---
+++ StringIntern/StringIntern.h
@@ -0,0 +1,18 @@
+//
+//  StringIntern.h
+//  StringIntern
+//
+//  Created by Galen Rhodes on 12/15/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringIntern.
+FOUNDATION_EXPORT double StringInternVersionNumber;
+
+//! Project version string for StringIntern.
+FOUNDATION_EXPORT const unsigned char StringInternVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringIntern/PublicHeader.h>
+
+
